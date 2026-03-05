ba5b7f8f700e382dfb998afda032541cd6abd896
시형 박 <parksh5317@gmail.com>
2026-03-05 18:22:45 +0900

    button1을 클릭하면 랜덤으로 배경색이 변경되게 하는 코드를 추가

--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -14,7 +16,9 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Random rand = new Random();
+            this.BackColor = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+        
         }
 
         private void label2_Click(object sender, EventArgs e)
